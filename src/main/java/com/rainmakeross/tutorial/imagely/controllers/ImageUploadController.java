a8598a3885f8e67ee5a50e6d457ab863083d9d7e
Daniel Yinanc <noemails@noemails.com>
2019-01-31 08:10:53 -0500

    PDF conversion enabled

--- src/main/java/com/rainmakeross/tutorial/imagely/controllers/ImageUploadController.java
+++ src/main/java/com/rainmakeross/tutorial/imagely/controllers/ImageUploadController.java
@@ -1,16 +1,17 @@
 package com.rainmakeross.tutorial.imagely.controllers;
 
+import com.itextpdf.text.DocumentException;
+import com.rainmakeross.tutorial.imagely.services.PdfService;
 import org.asynchttpclient.*;
 import org.asynchttpclient.request.body.multipart.FilePart;
 import org.asynchttpclient.request.body.multipart.StringPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import static org.asynchttpclient.Dsl.*;
 
 
@@ -31,8 +32,11 @@ public class ImageUploadController {
 
     Logger logger = LoggerFactory.getLogger(ImageUploadController.class);
 
+    @Autowired
+    PdfService pdfService;
+
     @PostMapping()
-    public ResponseEntity<?> receiveImage(@RequestBody String bodyString) throws ExecutionException, InterruptedException, IOException {
+    public ResponseEntity<?> receiveImage(@RequestBody String bodyString) throws ExecutionException, InterruptedException, IOException, DocumentException {
         String pwd =  System.getenv("MAILGUN_PWD");
         String username =  System.getenv("MAILGUN_NAME");
         String url = System.getenv("MAILGUN_URL");
@@ -40,17 +44,7 @@ public class ImageUploadController {
         String to = System.getenv("MAILGUN_TO");
 
 
-        String result = bodyString.split(",")[1];
-        Base64.Decoder decoder = Base64.getDecoder();
-        byte[] array = new byte[7]; // length is bounded by 7
-        new Random().nextBytes(array);
-        String generatedString = LocalDateTime.now() +".jpg";
-        byte[] bytes = decoder.decode(result);
-        try (OutputStream stream = new FileOutputStream(generatedString)) {
-            stream.write(bytes);
-        }
-
-        File imgFile = new File(generatedString);
+        File pdf = pdfService.createPDFFromImageString(bodyString);
 
         Realm realm = new Realm.Builder(username, pwd)
                 .setUsePreemptiveAuth(true)
@@ -62,13 +56,13 @@ public class ImageUploadController {
                 .addBodyPart(new StringPart("to",to))
                 .addBodyPart(new StringPart("subject","Ur image is contained"))
                 .addBodyPart(new StringPart("text", "Congratulations, you just sent yourself an image taken by your very own camera"))
-                .addBodyPart(new FilePart("inline",imgFile))
+                .addBodyPart(new FilePart("inline",pdf))
                 .build();
 
         Response r1 = asyncHttpClient.executeRequest(r).get();
 
         logger.info(r1.getStatusText());
-        imgFile.delete();
+        pdf.delete();
         return new ResponseEntity<>("All Good", HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/rainmakeross/tutorial/imagely/services/PdfService.java
@@ -0,0 +1,45 @@
+package com.rainmakeross.tutorial.imagely.services;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.util.Base64;
+
+@Service
+public class PdfService {
+
+    public File createPDFFromImageString(String imgString) throws IOException, DocumentException {
+        String result = imgString.split(",")[1];
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] bytes = decoder.decode(result);
+
+        String generatedString = LocalDateTime.now() +".pdf";
+        try (OutputStream stream = new FileOutputStream(generatedString)) {
+            stream.write(bytes);
+        }
+
+        FileOutputStream fos = new FileOutputStream(generatedString);
+
+        Document doc = new Document();
+
+        PdfWriter writer = PdfWriter.getInstance(doc, fos);
+        writer.open();
+
+        doc.open();
+        doc.add(Image.getInstance(bytes));
+        doc.close();
+
+        writer.close();
+        fos.close();
+
+        return new File(generatedString);
+    }
+}
